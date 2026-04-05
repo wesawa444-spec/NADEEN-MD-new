bc8f600c5e6f604fc665a340e11229a49b1c0896
wesawa444-spec <wesawa444@gmail.com>
2026-04-05 20:49:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'PRABATH-MD~Gwp6aXGfpGSiGTz' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Nadeen-MD~4cruO9970SkJxMf' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME
 };
